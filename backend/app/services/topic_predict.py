756b2a2fd26fc676e231c7347a6b5e40afbd6d6d
pop_ka098 <andrey.petrovich0880@@gmail.com>
2026-05-10 10:56:12 +0300

    changed model

--- backend/app/services/topic_predict.py
+++ backend/app/services/topic_predict.py
@@ -15,38 +15,26 @@ VER = ""
 model = joblib.load(MODELS_DIR / f"topic_model{VER}.pkl")
 
 IDX2WORD = {
-    0: 'alt.atheism',
-    1: 'comp.graphics',
-    2: 'comp.os.ms-windows.misc',
-    3: 'comp.sys.ibm.pc.hardware',
-    4: 'comp.sys.mac.hardware',
-    5: 'comp.windows.x',
-    6: 'misc.forsale',
-    7: 'rec.autos',
-    8: 'rec.motorcycles',
-    9: 'rec.sport.baseball',
-    10: 'rec.sport.hockey',
-    11: 'sci.crypt',
-    13: 'sci.med',
-    12: 'sci.electronics',
-    14: 'sci.space',
-    15: 'soc.religion.christian',
-    16: 'talk.politics.guns',
-    17: 'talk.politics.mideast',
-    18: 'talk.politics.misc',
-    19: 'talk.religion.misc',
+    1: "Society & Culture",
+    2: "Science & Mathematics",
+    3: "Health",
+    4: "Education & Reference",
+    5: "Computers & Internet",
+    6: "Sports",
+    7: "Business & Finance",
+    8: "Entertainment & Music",
+    9: "Family & Relationships",
+    10: "Politics & Government",
 }
 
 def clean_text(x):
+    if x is None:
+        x = ""
+
     x = str(x)
-    x = re.sub(r"-{3,}", " ", x)
-    x = re.sub(r"\S+@\S+", " ", x)
-    x = re.sub(r"\bDoD\s*#?\d+\b", " ", x)
-    x = re.sub(r"\".*?\"", " ", x)
-    x = re.sub(r"[^\w\s.,!?'/]", " ", x)
     x = re.sub(r"\s+", " ", x)
 
-    return x.strip().lower()
+    return x.strip()
 
 def topic_predict(text:str):
     text = clean_text(text)
